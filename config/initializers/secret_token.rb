8c4da59d4b7bdc8522486fbb8501d1babad6032c
Al Nahian <a@localhost.localdomain>
2013-08-08 18:31:47 +0600

    on fedora-18, added extension named spree_simple_sales, also added sale_variant using rails console after including minitest

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyStore::Application.config.secret_token = 'e9a8e38197ed23999086a0e6b2bdd9b91f9a45008cba3fe27580ef7b4a473915ec34e7e8af1c1ba88f2959e726bf4ba90349d5e6acfce97148cae4c1cadecd32'
